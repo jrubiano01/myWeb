71ca114a6d55b0be41e5c1dac27cc80e9ce83c4e
jrubiano01 <115517445+jrubiano01@users.noreply.github.com>
2024-01-20 13:02:28 -0500

    myWeb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatListModule, MatProgressSpinnerModule, MatSliderModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+/** Components */
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+import { TodoListComponent } from './views/todo-list/todo-list.component';
+
+export const routes: Routes = [
+    {
+        path: '',        
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'todo-list',
+        component: TodoListComponent
+    }
+];
